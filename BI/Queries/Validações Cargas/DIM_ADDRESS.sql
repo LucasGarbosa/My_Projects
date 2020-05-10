04e37f22de642e74904f9f130c2f228f1b9642bb
LucasGarbosa <lucas.garbosa@decode.pt>
2020-05-10 16:13:59 +0100

    Adjust Script

--- BI/Queries/Validações Cargas/DIM_ADDRESS.sql
+++ BI/Queries/Validações Cargas/DIM_ADDRESS.sql
@@ -3,21 +3,20 @@
 SELECT * FROM [dbo].[STG_Address]
 SELECT * FROM [dbo].DIM_Address
 
+--INSERT
 INSERT INTO [dbo].[STG_Address] (clusterName,DataCenter,Type1,Type2,Address,Number,Complement,PostalCode,City,Country,LoadDate)
-VALUES ('hoje','dia','de','testar','dados',99999999,'sempre','business','forever','just',GetDate())
+VALUES ('B','DataMin','Av','','João Maria',5678,'','2805-000','Almada','Portugal',GetDate())
 
-INSERT INTO [dbo].[DIM_Address]( FK_ID_Address,ClusterName,DataCenter,Type1,Type2,Address,Number,Complement,PostalCode,City,Country,StartDate, EndDate, LoadDate, Active, LoadStatus)
-VALUES (10,'a','a','a','a','a',1,'a','a','a','a',GetDate(),GetDate(),GetDate(),1,'U')
+INSERT INTO [dbo].[STG_Address] (clusterName,DataCenter,Type1,Type2,Address,Number,Complement,PostalCode,City,Country,LoadDate)
+VALUES ('A','DataMax','Praça','','João 23',1234,'Prox. Mercado','2805-322','Lisboa','Portugal',GetDate())
 
+--UPDATE
 UPDATE [dbo].[STG_Address]
 SET Number = 99
 WHERE ID_ADDRESS = 1
 
+--DELETE
 delete FROM [dbo].DIM_Address where SK_Address = 5
 delete FROM [dbo].STG_Address where Id_Address = 15
 
-truncate table [dbo].[DIM_Address]
-
-UPDATE DIM_ADDRESS 
-SET [EndDate] = GETDATE(), Active = 0, LoadStatus = 'U' 
-WHERE FK_ID_Address = 1 AND [EndDate] IS NULL
+--truncate table [dbo].[DIM_Address]
